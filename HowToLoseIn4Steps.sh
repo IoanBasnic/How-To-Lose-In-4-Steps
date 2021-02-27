4d2acee6939c1462a8022338de7953422365e006
Petrified0110 <petra.horvath99@gmail.com>
2021-02-27 18:01:55 +0200

    Add documentation

--- HowToLoseIn4Steps.sh
+++ HowToLoseIn4Steps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#To run this Shell script you need to have the lxterminal installed
+#run the 'sudo apt-get install -y lxterminal' to install it on Ubuntu
+
 gcc -Wall -o server Server/TCPserver.c
 gcc -Wall -o client Client/TCPclient.c
 
