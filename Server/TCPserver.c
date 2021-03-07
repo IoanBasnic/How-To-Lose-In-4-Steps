20a0055e6680312f0e12953d1a5e4f37bee0b8bf
Petrified0110 <petra.horvath99@gmail.com>
2021-03-07 18:53:16 +0200

    Resolve any server errors and segmentation faults

--- Server/TCPserver.c
+++ Server/TCPserver.c
@@ -5,6 +5,7 @@
 
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #include <netinet/in.h>
 
@@ -15,225 +16,227 @@ int key = 4;
 int **board;
 int column_full = 1, line, column = -1;
 int error_index = 0;
+char* messageToSend;
 
-void initializeBoard(){
-	board = (int**)malloc(sizeof(int*)*N);
+void setMessageToSend(int lineToSet, int columnToSet, int player, int error, int status){
+	free(messageToSend);
 
-	for(int i = 0; i < M; i++){
-		board[i] = (int*)malloc(sizeof(int) * M);
+	messageToSend = (char*)malloc(sizeof(char) * 9);
+	sprintf(messageToSend, "%d%d-%d-%d-%d", lineToSet, columnToSet, player, error, status);
+}
+
+void initializeBoard()
+{
+	board = (int **)malloc(sizeof(int *) * N);
+
+	for (int i = 0; i < M; i++)
+	{
+		board[i] = (int *)malloc(sizeof(int) * M);
 	}
 
-	for(int i = 0; i < N; i++)
-		for(int j = 0; j < M; j++)
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
 			board[i][j] = 0;
 }
 
 int is_board_full()
 {
-    int i = N-1;
-    int count = 0;
-    
-    for(int j=0; j<M; j++)
-    {
-        if(board[i][j] != 0)
-        {
-            count++;
-        }
-    }
-    
-    if(count == M)
-        return 1;
+	int i = N - 1;
+	int count = 0;
 
-    return 0;
+	for (int j = 0; j < M; j++)
+	{
+		if (board[i][j] != 0)
+		{
+			count++;
+		}
+	}
+
+	if (count == M)
+		return 1;
+
+	return 0;
 }
 
-int is_column_full(){
-	return board[0][column] =! 0;
+int is_column_full()
+{
+	return board[0][column] != 0;
 }
 
 void make_move(int player)
 {
-  for(int i=N-1; i>=0; i--)
-  {
-  	if(board[i][column] == 0)
-  	{
-      board[i][column] = player + 1; 
-      line = i;
-      return;
-  	}
-  }
+	for (int i = N - 1; i >= 0; i--)
+	{
+		if (board[i][column] == 0)
+		{
+			board[i][column] = player + 1;
+			line = i;
+			return;
+		}
+	}
 }
 
 int chech_win(int player)
 {
-	int i,j;
+	int i, j;
 
-    for(i=N-1;i>=0;i--)
-        {
-        	for(j=0;j<M-3;j++)
-        	{
-              if(board[i][j] != 0 && board[i][j]==board[i][j+1] && board[i][j]==board[i][j+2] && board[i][j]==board[i][j+3])
-              {
-                return 1;
-              }
-            }
-        }
- 
-    for(i=N-4;i>=0;i--)
-    {
-        for(j=0;j<M;j++)
-        {
-            if(board[i][j] != 0 && board[i][j]==board[i+1][j] && board[i][j]==board[i+2][j] && board[i][j]==board[i+3][j])
-            {
-              return 1;
-            }
-        }    
-    }   
- 
-    for(i=0;i<N-3;i++)
-    {
-        for(j=0;j<M-3;j++)
-        {
-            if(board[i][j] != 0 && board[i][j]==board[i+1][j+1] && board[i][j]==board[i+2][j+2] && board[i][j]==board[i+3][j+3])
-            {
-              return 1;
-            }
-        }
-    }
-    
-    for(i=0;i<N-3;i++)
-    {
-        for(j=3;j<M;j++)
-        {
-            if(board[i][j] != 0 && board[i][j]==board[i+1][j-1] && board[i][j]==board[i+2][j-2] && board[i][j]==board[i+3][j-3])
-            {
-              return 1;
-            }
-        }
-    }
+	for (i = N - 1; i >= 0; i--)
+	{
+		for (j = 0; j < M - 3; j++)
+		{
+			if (board[i][j] != 0 && board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] && board[i][j] == board[i][j + 3])
+			{
+				return 1;
+			}
+		}
+	}
 
-    return 0;
+	for (i = N - 4; i >= 0; i--)
+	{
+		for (j = 0; j < M; j++)
+		{
+			if (board[i][j] != 0 && board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j] && board[i][j] == board[i + 3][j])
+			{
+				return 1;
+			}
+		}
+	}
+
+	for (i = 0; i < N - 3; i++)
+	{
+		for (j = 0; j < M - 3; j++)
+		{
+			if (board[i][j] != 0 && board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2] && board[i][j] == board[i + 3][j + 3])
+			{
+				return 1;
+			}
+		}
+	}
+
+	for (i = 0; i < N - 3; i++)
+	{
+		for (j = 3; j < M; j++)
+		{
+			if (board[i][j] != 0 && board[i][j] == board[i + 1][j - 1] && board[i][j] == board[i + 2][j - 2] && board[i][j] == board[i + 3][j - 3])
+			{
+				return 1;
+			}
+		}
+	}
+
+	return 0;
 }
 
 int input_validation(char *input)
 {
-  char choice;
-  
-  if(strlen(input) != 1)
-  	return 1;
-    
-   sscanf(input, "%c", &choice);
+	char choice;
 
-	if(isdigit(choice))
-    	return 2;
-   
-	if(!((choice == 'A') || (choice == 'B') || (choice == 'C') || (choice == 'D') || 
-	  (choice == 'E') || (choice == 'F') || (choice == 'G')))
-    	return 2;
+	if (strlen(input) != 1)
+		return 1;
 
-   column = choice - 'A';
+	sscanf(input, "%c", &choice);
 
-   if(is_column_full())
-   {
+	if (isdigit(choice))
+		return 2;
+
+	if (!((choice == 'A') || (choice == 'B') || (choice == 'C') || (choice == 'D') ||
+				(choice == 'E') || (choice == 'F') || (choice == 'G')))
+		return 2;
+
+	column = choice - 'A';
+
+	if (is_column_full())
+	{
 		column = -1;
-     	return 3;
-   }
+		return 3;
+	}
 
-   return 0;
+	return 0;
 }
 
-
-char * encryptMessageServer(char * string)
+char *encryptMessageServer(char *string)
 {
-	char *message = malloc(4*sizeof(char * ));;
+	char *message = malloc(4 * sizeof(char *));
+	;
 	memcpy(message, string, strlen(string));
 	char aux;
 
-	
-	for(int i = 0; message[i] != '\0'; i++)
+	for (int i = 0; message[i] != '\0'; i++)
 	{
 		aux = message[i];
-		
-		if(aux >= '0' && aux <= '9')
-		{	
-			
+
+		if (aux >= '0' && aux <= '9')
+		{
+
 			aux = aux + key;
-			
-			if(aux > '9')
+
+			if (aux > '9')
 			{
 
 				aux = aux - '9' + '0' - 1;
 			}
 
 			message[i] = aux;
-
 		}
 
-		else if(aux =='-')
+		else if (aux == '-')
 		{
 			aux = aux + key;
-			
-			if(aux > '-')
+
+			if (aux > '-')
 			{
 				aux = aux + '-' - 1;
 			}
 
 			message[i] = aux;
-			
 		}
 	}
 
-	
 	return message;
 }
 
-char * decryptMessageServer(char * string)
+char *decryptMessageServer(char *string)
 {
 
-	
-	char *message = malloc(4*sizeof(char * ));
+	char *message = malloc(4 * sizeof(char *));
 	strcpy(message, string);
 	char aux;
 
-	
-	for(int i = 0; message[i] != '\0'; i++)
+	for (int i = 0; message[i] != '\0'; i++)
 	{
 		aux = message[i];
-		
-		if(aux >= 'a' && aux <= 'z')
-		{	
-			
+
+		if (aux >= 'a' && aux <= 'z')
+		{
+
 			aux = aux - key;
-			
-			if(aux < 'a')
+
+			if (aux < 'a')
 			{
 
 				aux = aux + 'z' - 'a' + 1;
 			}
 
 			message[i] = aux;
-
 		}
 
-		else if(aux >= 'A' && aux <= 'Z')
+		else if (aux >= 'A' && aux <= 'Z')
 		{
 			aux = aux - key;
-			if(aux < 'A')
+			if (aux < 'A')
 			{
 				aux = aux + 'Z' - 'A' + 1;
 			}
 
 			message[i] = aux;
-			
 		}
 	}
 
-	
 	return message;
 }
 
-
-int main(void) {
+int main(void)
+{
 	//create the server socket
 	int server_socket;
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -245,14 +248,15 @@ int main(void) {
 	server_address.sin_addr.s_addr = INADDR_ANY;
 
 	//bind the socket t our specified IP and port
-	bind(server_socket, (struct sockaddr*) & server_address, sizeof(server_address));
+	bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
 
 	listen(server_socket, 2);
 
 	int client_fd[2];
 
 	//First we send each client the allocated to number, so they know which client they are
-	for (int i = 0; i < 2; i++){
+	for (int i = 0; i < 2; i++)
+	{
 		client_fd[i] = accept(server_socket, NULL, NULL);
 		int message = i;
 		send(client_fd[i], &message, sizeof(message), 0);
@@ -263,69 +267,70 @@ int main(void) {
 	int otherPlayer = 1;
 	initializeBoard();
 
-	while(responseCode[0] != 0 && responseCode[1] != 0) {
-		char* messageToSend = "00-0-0-0";
-		char* messageToReceive = (char*)malloc(sizeof(char));
+	while (responseCode[0] != 0 && responseCode[1] != 0)
+	{
+		char *messageToReceive = (char *)malloc(sizeof(char) * 255);
 
-		if(is_board_full()){
+		if (is_board_full())
+		{
 			messageToSend[5] = (char)(0);
 			messageToSend[7] = (char)(3);
-			for(int i = 0; i < 2; i++){
+			for (int i = 0; i < 2; i++)
+			{
 				send(client_fd[i], &messageToSend, sizeof(messageToSend), 0);
 			}
-			pclose(&server_socket);
+			close(server_socket);
 			return 0;
 		}
 
 		//We wait for a valid message from the player
 		int error_index = 0;
-		do{
-			printf("%s\n", messageToSend);
-			printf("turn : %d\n", whoseTurn);
-			responseCode[whoseTurn] = recv(client_fd[whoseTurn], messageToReceive, sizeof(char), 0);
-			printf("got here\n");
+		do
+		{
+			recv(client_fd[whoseTurn], messageToReceive, sizeof(char), 0);
 			messageToReceive = decryptMessageServer(messageToReceive);
-			printf("%c\n", messageToReceive);
 			error_index = input_validation(messageToReceive);
 
-			if(error_index){
-				messageToSend[3] = (char)(whoseTurn + 1);
-				messageToSend[5] = (char)(error_index);
+			if (error_index)
+			{
+				setMessageToSend(0, 0, whoseTurn+1, error_index, 0);
+				printf("%s\n", messageToSend);
 
 				send(client_fd[whoseTurn], &messageToSend, sizeof(messageToSend), 0);
 			}
 
-		} while(error_index != 0);
+		} while (error_index != 0);
 
 		make_move(whoseTurn);
 
-		if(chech_win(whoseTurn)){
-			messageToSend[7] = (char)(whoseTurn + 1);
-			for(int i = 0; i < 2; i++){
+		if (chech_win(whoseTurn))
+		{
+			setMessageToSend(0, 0, 0, 0, whoseTurn+1);
+			
+			for (int i = 0; i < 2; i++)
+			{
 				send(client_fd[i], &messageToSend, sizeof(messageToSend), 0);
 			}
-			pclose(&server_socket);
+			close(server_socket);
 			return 0;
 		}
 
-		if(line == -1 || column == -1){
-			messageToSend[5] = (char)(6);
-			for(int i = 0; i < 2; i++){
+		if (line == -1 || column == -1)
+		{
+			setMessageToSend(0, 0, 0, 6, 0);
+			for (int i = 0; i < 2; i++)
+			{
 				send(client_fd[i], &messageToSend, sizeof(messageToSend), 0);
 			}
-			pclose(&server_socket);
+			close(server_socket);
 			return 0;
 		}
-		else{
-
-			messageToSend[0] = (char)(line);
-			messageToSend[1] = (char)(column);
-			messageToSend[3] = (char)(whoseTurn + 1);
-			messageToSend[5] = (char)(0);
-			messageToSend[7] = (char)(0);
+		else
+		{
+			setMessageToSend(line, column, whoseTurn+1, 0, 0);
 			send(client_fd[otherPlayer], &messageToSend, sizeof(messageToSend), 0);
 			send(client_fd[whoseTurn], &messageToSend, sizeof(messageToSend), 0);
-					
+
 			column = -1;
 			line = -1;
 
@@ -333,7 +338,7 @@ int main(void) {
 			whoseTurn = otherPlayer;
 			otherPlayer = aux;
 		}
-        /* if not player turn -> error_index = 6
+		/* if not player turn -> error_index = 6
          * input_validation(message); v
          * if(!is_board_full()) v
          * {
@@ -352,20 +357,22 @@ int main(void) {
          */
 	}
 
-	if(responseCode[0] == 0){
-		char* message = "00-2-4-4";
-		char* encryptedMessage = encryptMessageServer(message);
+	if (responseCode[0] == 0)
+	{
+		char *message = "00-2-4-4";
+		char *encryptedMessage = encryptMessageServer(message);
 
 		send(client_fd[1], &encryptedMessage, sizeof(encryptedMessage), 0);
 	}
-	else if(responseCode[1] == 0){
-		char* message = "00-1-4-4";
-		char* encryptedMessage = encryptMessageServer(message);
+	else if (responseCode[1] == 0)
+	{
+		char *message = "00-1-4-4";
+		char *encryptedMessage = encryptMessageServer(message);
 
 		send(client_fd[0], &encryptedMessage, sizeof(encryptedMessage), 0);
 	}
 
-	pclose(server_socket);
+	close(server_socket);
 
 	return 0;
 }
